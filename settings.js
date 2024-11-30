e89cfd47600e2f76fb849c539a5fb31f2f6edbf9
rashmi688 <nethminiwijesinghe9@gmail.com>
2024-12-01 00:38:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ?'𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=C7BgjRiA#hgo5nT6BY7_FxMbt3jMSP_0GJ_R0co-sUeR55X4l-Fw
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
